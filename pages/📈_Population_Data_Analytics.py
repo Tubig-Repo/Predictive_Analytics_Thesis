18dff8d58696f815839f536a2f71ce05b85390b1
Carlo <45724144+Tubig-Repo@users.noreply.github.com>
2024-11-29 07:13:12 +0800

    removed ability to select base on municipality and other sub category

--- pages/📈_Population_Data_Analytics.py
+++ pages/📈_Population_Data_Analytics.py
@@ -25,16 +25,17 @@ option_island = st.selectbox(
       ("Luzon" , "Visayas", "Mindanao")
 )
 # Choose on wether to choose from Province/District, Regions or Municipalities
-option_level = st.selectbox(
-        "Select Base on Region or Province",
-        ("Region", "Province/Districts", "Municipality")
-)
+# option_level = st.selectbox(
+#         "Select Base on Region or Province",
+#         ("Region", "Province/Districts", "Municipality")
+# )
 # Choose base on cencsus year 
 option_census_year = st.selectbox(
     "Select Population Census",
     ("Population Census 1990" , "Population Census 2000" , "Population Census 2010", "Population Census 2015", "Population Census 2020")
 )
 # Load Data
+option_level = "Region"
 geo_data = data_loader.load_geojson(option_level=option_level)
 
 population_data = data_loader.load_population(option_census_year, option_level=option_level, option_island=option_island)
